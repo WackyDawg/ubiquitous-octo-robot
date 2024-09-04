2bae07ca9f04a4933e97251b7d7da99b12969a6f
Julian Nwadinobi <juliannwadinobi098@gmail.com>
2024-09-04 12:39:45 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const fs = require('fs')
 
 const app = express();
-const PORT = 4000;
+const PORT = 9806;
 
 app.get('/', (req, res) => {
   res.send({ message: "Server running" })
